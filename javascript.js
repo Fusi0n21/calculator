db93c7ca7a459bb9f9d8b690acdb8b9e1aa00f3e
Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
2023-02-03 11:47:20 +0200

    basic calculator

---
+++ javascript.js
@@ -0,0 +1,137 @@
+function add(a, b){
+    return +a + +b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+
+
+function operate(a, b, operator){
+    let result
+    switch(operator){
+        case 'add':
+            result = add(a, b)
+            populateDisplay(result, true)
+            break
+        case 'subtract':
+            result = subtract(a, b)
+            populateDisplay(result, true)
+            break
+        case 'multiply':
+            result = Math.round(multiply(a, b) * 100) / 100;
+            populateDisplay(result, true)
+            break
+        case 'divide':
+            result = Math.round(divide(a, b) * 100) / 100;
+            populateDisplay(result, true)
+            break
+    }
+
+
+}
+
+let displayValue = 0
+let firstValue = 0
+let operator = ''
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+
+for(let a of buttons){
+    a.addEventListener('click', onClick)
+}
+
+function onClick(){
+
+
+    switch(this.classList.value){
+        case 'zero':
+            populateDisplay('0')
+            break;
+        case 'one':
+            populateDisplay('1')
+            break
+        case 'two':
+            populateDisplay('2')
+            break
+        case 'three':
+            populateDisplay('3')
+            break
+        case 'four':
+            populateDisplay('4')
+            break;
+        case 'five':
+            populateDisplay('5')
+            break
+        case 'six':
+            populateDisplay('6')
+            break
+        case 'seven':
+            populateDisplay('7')
+            break
+        case 'eight':
+            populateDisplay('8')
+            break;
+        case 'nine':
+            populateDisplay('9')
+            break
+        case 'ac':
+            clearAC()
+            break
+        case 'add':
+            storeFirstValue()
+            operator = 'add'
+            break
+        case 'equals':
+            operate(firstValue, displayValue, operator)
+            break;
+        case 'subtract':
+            storeFirstValue()
+            operator = 'subtract'
+            break
+        case 'multiply':
+            storeFirstValue()
+            operator = 'multiply'
+            break
+        case 'divide':
+            storeFirstValue()
+            operator = 'divide'
+            break         
+
+    }
+}
+
+
+function populateDisplay(number, testLength = false){
+    if(!testLength){
+        if(display.innerText.length < 9){
+
+            display.innerText += number
+            displayValue = display.innerText
+        }
+    }else{
+        display.innerText = number
+        displayValue = display.innerText
+    }
+}
+
+function clearAC(){
+    display.innerText = null
+    displayValue = null
+    firstValue = null
+}
+
+function storeFirstValue(){
+    firstValue = displayValue
+    display.innerText = null
+    displayValue = null
+}
