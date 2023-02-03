bcd74a12b0afca4be4e8f84e1343c6305571d029
Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
2023-02-03 23:00:51 +0200

    Squashed commit of the following:
    commit 793c26ee22abd262f3d6eaafabae7b6ccc20f030
    Author: Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
    Date:   Fri Feb 3 22:54:17 2023 +0200
    
        Format
    
    commit 5612088062c4d374ec8ab045537da069f00c171c
    Author: Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
    Date:   Fri Feb 3 22:50:38 2023 +0200
    
        Format with Prettier
    
    commit 3a9944141ccad4b9acae94998f193686b74dfc45
    Author: Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
    Date:   Fri Feb 3 22:43:38 2023 +0200
    
        Fixed bug
        After pressing AC and entering next operator, the display printed 0
    
    commit a73f391190ea67920d1bf3070725623326f20146
    Author: Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
    Date:   Fri Feb 3 22:30:45 2023 +0200
    
        Added error for trying to divide by zero
    
    commit 7c5695a888f5ed4e597826126628a3f8d21feac6
    Author: Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
    Date:   Fri Feb 3 22:20:22 2023 +0200
    
        All operations working perfectly
    
    commit 6dcdc00251011cf9dd8c85563a955488e1aed9eb
    Author: Fusi0n21 <123664027+Fusi0n21@users.noreply.github.com>
    Date:   Fri Feb 3 21:56:50 2023 +0200
    
        Bug fix
        After pressing operator the display waits for a number input to clear it

--- javascript.js
+++ javascript.js
@@ -1,137 +1,169 @@
-function add(a, b){
-    return +a + +b
+function add(a, b) {
+	return +a + +b;
 }
 
-function subtract(a, b){
-    return a - b
+function subtract(a, b) {
+	return a - b;
 }
 
-function divide(a, b){
-    return a / b
+function divide(a, b) {
+	return a / b;
 }
 
-function multiply(a, b){
-    return a * b
+function multiply(a, b) {
+	return a * b;
 }
 
+function operate(a, b, operator) {
+	switch (operator) {
+		case "add":
+			result = Math.round(add(a, b) * 100) / 100;
+			populateDisplay(result, true);
+			break;
+		case "subtract":
+			result = Math.round(subtract(a, b) * 100) / 100;
+			populateDisplay(result, true);
+			break;
+		case "multiply":
+			result = Math.round(multiply(a, b) * 100) / 100;
+			populateDisplay(result, true);
+			break;
+		case "divide":
+			result = Math.round(divide(a, b) * 100) / 100;
+			populateDisplay(result, true);
+			break;
+	}
+	shouldClearDisplay = true;
+	allowToCalc = false;
+}
+let result;
+let allowToCalc = false;
+let shouldClearDisplay = false;
+let displayValue = 0;
+let firstValue = 0;
+let operator = "";
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
 
-
-function operate(a, b, operator){
-    let result
-    switch(operator){
-        case 'add':
-            result = add(a, b)
-            populateDisplay(result, true)
-            break
-        case 'subtract':
-            result = subtract(a, b)
-            populateDisplay(result, true)
-            break
-        case 'multiply':
-            result = Math.round(multiply(a, b) * 100) / 100;
-            populateDisplay(result, true)
-            break
-        case 'divide':
-            result = Math.round(divide(a, b) * 100) / 100;
-            populateDisplay(result, true)
-            break
-    }
-
-
+for (let a of buttons) {
+	a.addEventListener("click", onClick);
 }
 
-let displayValue = 0
-let firstValue = 0
-let operator = ''
-const display = document.querySelector('.display')
-const buttons = document.querySelectorAll('button')
+function onClick() {
+	switch (this.classList.value) {
+		case "zero":
+			populateDisplay("0");
+			break;
+		case "one":
+			populateDisplay("1");
+			break;
+		case "two":
+			populateDisplay("2");
+			break;
+		case "three":
+			populateDisplay("3");
+			break;
+		case "four":
+			populateDisplay("4");
+			break;
+		case "five":
+			populateDisplay("5");
+			break;
+		case "six":
+			populateDisplay("6");
+			break;
+		case "seven":
+			populateDisplay("7");
+			break;
+		case "eight":
+			populateDisplay("8");
+			break;
+		case "nine":
+			populateDisplay("9");
+			break;
+		case "ac":
+			clearAC();
+			break;
+		case "add":
+			if (allowToCalc) {
+				operate(firstValue, displayValue, operator);
+			}
+			storeFirstValue();
 
-for(let a of buttons){
-    a.addEventListener('click', onClick)
+			operator = "add";
+			break;
+		case "subtract":
+			if (allowToCalc) {
+				operate(firstValue, displayValue, operator);
+			}
+			storeFirstValue();
+			operator = "subtract";
+			break;
+		case "multiply":
+			if (allowToCalc) {
+				operate(firstValue, displayValue, operator);
+			}
+			storeFirstValue();
+			operator = "multiply";
+			break;
+		case "divide":
+			if (allowToCalc) {
+				operate(firstValue, displayValue, operator);
+			}
+			storeFirstValue();
+			operator = "divide";
+			break;
+		case "equals":
+			operate(firstValue, displayValue, operator);
+			break;
+	}
+
+	for (let a of buttons) {
+		if (a.classList.value == "percent") {
+			a.innerText = operator;
+		}
+	}
 }
 
-function onClick(){
-
-
-    switch(this.classList.value){
-        case 'zero':
-            populateDisplay('0')
-            break;
-        case 'one':
-            populateDisplay('1')
-            break
-        case 'two':
-            populateDisplay('2')
-            break
-        case 'three':
-            populateDisplay('3')
-            break
-        case 'four':
-            populateDisplay('4')
-            break;
-        case 'five':
-            populateDisplay('5')
-            break
-        case 'six':
-            populateDisplay('6')
-            break
-        case 'seven':
-            populateDisplay('7')
-            break
-        case 'eight':
-            populateDisplay('8')
-            break;
-        case 'nine':
-            populateDisplay('9')
-            break
-        case 'ac':
-            clearAC()
-            break
-        case 'add':
-            storeFirstValue()
-            operator = 'add'
-            break
-        case 'equals':
-            operate(firstValue, displayValue, operator)
-            break;
-        case 'subtract':
-            storeFirstValue()
-            operator = 'subtract'
-            break
-        case 'multiply':
-            storeFirstValue()
-            operator = 'multiply'
-            break
-        case 'divide':
-            storeFirstValue()
-            operator = 'divide'
-            break         
-
-    }
+function populateDisplay(number, testLength = false) {
+	if (shouldClearDisplay) {
+		display.innerText = null;
+		displayValue = null;
+		shouldClearDisplay = false;
+	}
+	allowToCalc = true;
+	if (!testLength) {
+		if (display.innerText.length < 9) {
+			display.innerText += number;
+			displayValue = display.innerText;
+		}
+	} else {
+		if (result == Infinity || result == NaN) {
+			console.log("a");
+			display.innerText = "wtf";
+		} else {
+			display.innerText = number;
+			displayValue = display.innerText;
+		}
+	}
 }
 
-
-function populateDisplay(number, testLength = false){
-    if(!testLength){
-        if(display.innerText.length < 9){
-
-            display.innerText += number
-            displayValue = display.innerText
-        }
-    }else{
-        display.innerText = number
-        displayValue = display.innerText
-    }
+function clearAC() {
+	display.innerText = null;
+	displayValue = "";
+	firstValue = null;
+	result = null;
+	allowToCalc = false;
+	shouldClearDisplay = false;
+	operator = "";
+	for (let a of buttons) {
+		if (a.classList.value == "percent") {
+			a.innerText = "";
+		}
+	}
 }
 
-function clearAC(){
-    display.innerText = null
-    displayValue = null
-    firstValue = null
-}
-
-function storeFirstValue(){
-    firstValue = displayValue
-    display.innerText = null
-    displayValue = null
+function storeFirstValue() {
+	firstValue = displayValue;
+	shouldClearDisplay = true;
 }
